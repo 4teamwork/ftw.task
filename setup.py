2cafcdfe4832e7377494e558e88f7934302b6a6d
Julian Infanger <j.infanger@4teamwork.ch>
2009-08-05 14:57:31 +0000

    moved version from setup.py to version.txt

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '0.2-dev'
+version = open('izug/task/version.txt').read().strip()
 maintainer = 'Victor Baumann'
 
 long_description = (
