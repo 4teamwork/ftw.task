60645362636ace6b92ed93751ee8e997405749bb
Jonas Baumann <j.baumann@4teamwork.ch>
2010-06-22 09:38:02 +0000

    Fixed import error in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@
 """
 This module contains the tool of izug.task
 """
-import os
 from setuptools import setup, find_packages
 
 def read(*rnames):
-    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+    return open('/'.join(rnames)).read()
 
 version = open('izug/task/version.txt').read().strip()
 maintainer = 'Victor Baumann'
