dfa6cb2104d8694c8bd6bc29d0cb46ef7fbc81ac
Jonas Baumann <jone@jone.ch>
2012-03-15 13:44:43 +0100

    Move version to setup.py ; cleanup setup.py.

--- setup.py
+++ setup.py
@@ -1,31 +1,8 @@
-# -*- coding: utf-8 -*-
-"""
-This module contains the tool of ftw.task
-"""
 from setuptools import setup, find_packages
+import os
 
-def read(*rnames):
-    return open('/'.join(rnames)).read()
-
-version = open('ftw/task/version.txt').read().strip()
-maintainer = 'Victor Baumann'
-
-long_description = (
-    read('README.txt')
-    + '\n' +
-    'Change history\n'
-    '**************\n'
-    + '\n' +
-    read('docs', 'HISTORY.txt')
-    + '\n' +
-    'Detailed Documentation\n'
-    '**********************\n'
-    + '\n' +
-    read('ftw', 'task', 'README.txt')
-    + '\n' +
-    'Download\n'
-    '********\n'
-    )
+version = '2.3.2.dev0'
+maintainer = 'Mathias Leimgruber'
 
 tests_require = [
     'collective.testcaselayer',
@@ -42,31 +19,43 @@ extras_require = {
 
 setup(name='ftw.task',
       version=version,
-      description="Task type (Maintainer: %s)" % maintainer,
-      long_description=long_description,
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      description='A simple task content type for plone.',
+      long_description=open('README.txt').read() + '\n' + \
+          open(os.path.join('docs', 'HISTORY.txt')).read(),
+
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         'Framework :: Plone',
-        'Intended Audience :: Developers',
+        'Framework :: Plone :: 4.1',
+        'Framework :: Plone :: 4.2',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
         ],
-      keywords='plone archetype ftw',
-      author='%s, 4teamwork GmbH' % maintainer,
+
+      keywords='ftw task plone',
+      author='4teamwork GmbH',
       author_email='mailto:info@4teamwork.ch',
       maintainer=maintainer,
-      url='http://psc.4teamwork.ch/4teamwork/kunden/ftw/ftw.task/',
+      url='',
       license='GPL2',
+
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['ftw', ],
       include_package_data=True,
       zip_safe=False,
-      install_requires=['setuptools',
+
+      install_requires=[
+        'setuptools',
         'Products.ATReferenceBrowserWidget',
-                        'plone.principalsource',
-                        # -*- Extra requirements: -*-
-                        ],
+        'plone.principalsource',
+        ],
+
       tests_require=tests_require,
       extras_require=extras_require,
+
       test_suite = 'ftw.task.tests.test_docs.test_suite',
       entry_points="""
       # -*- Entry points: -*-
